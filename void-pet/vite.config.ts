07c8bdf574daf3c609a3f676a827c48087c7b14a
Claudia <149967204+claudiahodoroga@users.noreply.github.com>
2025-04-25 15:38:47 +0200

    more configs

--- void-pet/vite.config.ts
+++ void-pet/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // for api calls to be proxied to local azure functions
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7071',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
