c0cea37e696a62b31386851fd79dcc38daf01fb6
Claudia <149967204+claudiahodoroga@users.noreply.github.com>
2025-06-08 21:30:29 +0200

    Update ComputerShell.tsx

--- voidpet-project/src/components/ComputerShell/ComputerShell.tsx
+++ voidpet-project/src/components/ComputerShell/ComputerShell.tsx
@@ -18,26 +18,16 @@ export const TopBar: React.FC<TopBarProps> = ({
         {/* Elige UNO de los siguientes SVGs. Descomenta el que quieras usar y comenta o elimina los otros. */}
 
         {/* --- Opción 1: Huevo Simple (Ovalado) --- */}
-        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={styles.topBarIcon}>
-          <path d="M12 2C8.13 2 5 6.03 5 11c0 4.97 3.13 9 7 9s7-4.03 7-9c0-4.97-3.13-9-7-9z" />
-        </svg> */}
-
-        {/* --- Opción 2: Huevo Ligeramente Roto (Descomenta para usar) --- */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className={styles.topBarIcon}
-        >
-          <path d="M12 2C8.13 2 5 6.03 5 11c0 2.3.88 4.4 2.33 6.01l-1.11 1.11L7.7 19.6l1.48-1.48.99.99 1.11-1.11.72.72 1.11-1.11.99.99 1.48-1.48 1.48 1.48-1.11 1.11C18.12 15.4 19 13.3 19 11c0-4.97-3.13-9-7-9z" />
-        </svg>
-
-        {/* --- Opción 3: Huevo con Criatura Asomándose (Descomenta para usar) --- */}
-        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={styles.topBarIcon}>
-          <path d="M12 2C8.13 2 5 6.03 5 11c0 2.3.88 4.4 2.33 6.01l-1.11 1.11L7.7 19.6l1.48-1.48.99.99 1.11-1.11.72.72 1.11-1.11.99.99 1.48-1.48 1.48 1.48-1.11 1.11C18.12 15.4 19 13.3 19 11c0-4.97-3.13-9-7-9zm-1-5a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0z" clip-rule="evenodd" />
-          <path d="M12 7a1 1 0 100-2 1 1 0 000 2z" />
-        </svg>
-        */}
+        {
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className={styles.topBarIcon}
+          >
+            <path d="M12 2C8.13 2 5 6.03 5 11c0 4.97 3.13 9 7 9s7-4.03 7-9c0-4.97-3.13-9-7-9z" />
+          </svg>
+        }
 
         {showTitle && (
           <span className={styles.topBarTitle}>
